bb96b025a6c219a4521a4f067b8d4112b526ca73
howon-ryu <qorgh2akfl@naver.com>
2023-09-11 18:00:41 +0900

    mteb start

---
+++ mteb_start.py
@@ -0,0 +1,13 @@
+from mteb import MTEB
+from sentence_transformers import SentenceTransformer
+
+# Define the sentence-transformers model name
+#model_name = "average_word_embeddings_komninos"
+model_name = "bert-base-uncased"
+
+model = SentenceTransformer(model_name)
+evaluation = MTEB(tasks=["Banking77Classification"])
+results = evaluation.run(model, output_folder=f"results/{model_name}")
+
+
+print(results)
