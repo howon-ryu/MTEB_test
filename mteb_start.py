9b558a1a522bd88922002c175ad90b61258cf2b0
howon-ryu <qorgh2akfl@naver.com>
2023-09-12 11:04:04 +0900

    model test

--- mteb_start.py
+++ mteb_start.py
@@ -3,8 +3,12 @@ from sentence_transformers import SentenceTransformer
 
 # Define the sentence-transformers model name
 #model_name = "average_word_embeddings_komninos"
-model_name = "bert-base-uncased"
-
+# model_name = "bert-base-uncased"
+# model_name = "BM-K/KoMiniLM"
+# model_name = "BM-K/KoChatBART"
+# model_name = "thenlper/gte-base"
+#model_name = "intfloat/e5-small-v2"
+model_name = "BAAI/bge-base-en"
 model = SentenceTransformer(model_name)
 evaluation = MTEB(tasks=["Banking77Classification"])
 results = evaluation.run(model, output_folder=f"results/{model_name}")
