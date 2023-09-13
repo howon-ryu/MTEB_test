b1d8ab3a32dc6af6a9fc5fd572befdafb3873199
howon-ryu <qorgh2akfl@naver.com>
2023-09-13 09:17:39 +0900

    230913

--- mteb_start.py
+++ mteb_start.py
@@ -8,9 +8,20 @@ from sentence_transformers import SentenceTransformer
 # model_name = "BM-K/KoChatBART"
 # model_name = "thenlper/gte-base"
 #model_name = "intfloat/e5-small-v2"
-model_name = "BAAI/bge-base-en"
+# model_name = "BAAI/bge-base-en"
+# model_name = "google/flan-t5-base"
+# model_name = "t5-base"
+model_name = "quantumaikr/KoreanLM"
+
 model = SentenceTransformer(model_name)
-evaluation = MTEB(tasks=["Banking77Classification"])
+#evaluation = MTEB(tasks=["Banking77Classification"])#Classification
+# evaluation = MTEB(tasks=["BUCC"]) # Bitext
+#evaluation = MTEB(tasks=["MedrxivClusteringP2P"])#Clustering
+# evaluation = MTEB(tasks=["TwitterURLCorpus"])#Pair Classification
+# evaluation = MTEB(tasks=["MindSmallReranking"])#Reranking
+# evaluation = MTEB(tasks=["ArguAna"])#Retrival
+# evaluation = MTEB(tasks=["BIOSSES"])#STS
+# evaluation = MTEB(tasks=["SummEval"])#Summarization
 results = evaluation.run(model, output_folder=f"results/{model_name}")
 
 
